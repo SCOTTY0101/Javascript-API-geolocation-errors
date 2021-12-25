c7cd8e67525800897756d199ec8b29ec07c90f80
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2021-12-25 13:28:29 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 /*javascrpit code to run an API on gps location. With handling of errors.
 Running on ES6. 
+Code still under development.....
 */
 
 getLocation = () => {
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
-        console.log("Geolocation is not supported by this browser.");
+        console.log('Geolocation is not supported by this browser.');
     }
-}
+};
 
 showPosition = (position) => {
-    console.log(`Latitude: ${position.coords.latitude}
+    console.log(`Latitude: ${position.coords.latitude};
     Longitude: ${position.coords.lonitude}`);
-}
+};
+
 showError = (error) => {
     switch(error.code) {
         case error.PERMISSION_DENIED:
-            console.log("User denied the request for Geolocation.");
+            console.log('User denied the request for Geolocation.');
             break;
         case error.POSITION_UNAVAILABLE:
-            console.log("Location information is unavailable.");
+            console.log('Location information is unavailable.');
             break;
         case error.TIMEOUT:
-            console.log("The requset to get user location timed out.");
+            console.log('The requset to get user location timed out.');
             break;
         case error.UNKNOWN_ERROR:
-            console.log("An unknown error occurred.");
+            console.log('An unknown error occurred.');
             break;
         default:
-            console.log("best read and change this code to remove me :p");
+            console.log('best read and change this code to remove me :p');
     }
-}
+};
